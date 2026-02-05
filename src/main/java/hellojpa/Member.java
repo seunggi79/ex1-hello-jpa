58c3d9eb07476e3b896be61c6940232027aec125
seunggi <hsg77689469@gmail.com>
2026-02-06 00:35:02 +0900

    데이터베이스 스키마 자동 생성

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,5 +1,6 @@
 package hellojpa;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
@@ -8,7 +9,9 @@ public class Member {
 
     @Id
     private Long id;
+    @Column(unique = true, length = 10) 
     private String name;
+    private int age;
 
     public Member(){
 
