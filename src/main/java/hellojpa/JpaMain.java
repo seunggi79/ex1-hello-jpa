c424f192e1b2370412b63f5964b8994ea5a430db
seunggi <hsg77689469@gmail.com>
2026-02-06 01:33:34 +0900

    필드와 컬럼 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,15 +17,6 @@ public class JpaMain {
         tx.begin();
 
         try{
-            Member member = new Member(200L, "NNN");
-            em.persist(member);
-
-            // 영속
-            member = em.find(Member.class, 200L);
-            member.setName("AAA");
-            em.detach(member);
-
-
             System.out.println("=============");
             tx.commit();
         }catch (Exception e){
