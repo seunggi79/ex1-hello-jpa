8b875f826bd9f4a5688cfb8b1ef8a879cb07bc9d
seunggi <hsg77689469@gmail.com>
2026-02-05 20:26:27 +0900

    영속성 컨텍스트 1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,8 +1,10 @@
 package hellojpa;
 
-import jakarta.persistence.*;
 
-import java.util.List;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 
 public class JpaMain {
 
@@ -15,10 +17,16 @@ public class JpaMain {
         tx.begin();
 
         try{
-            List<Member> result = em.createQuery("select m from Member as m", Member.class).getResultList();
-            for (Member member : result){
-                System.out.println("member.name = " + member.getName());
-            }
+
+            // 비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
+
+            // 영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
 
             tx.commit();
         }catch (Exception e){
