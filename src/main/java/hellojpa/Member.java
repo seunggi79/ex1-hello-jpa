62b906169e4599e133f3ea21c94de52a6d3b5f12
seunggi <hsg77689469@gmail.com>
2026-02-06 17:27:45 +0900

    단방향 연관관계

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -5,22 +5,45 @@ import jakarta.persistence.*;
 import java.util.Date;
 @Entity
 public class Member {
-    @Id
+    @Id @GeneratedValue
     private Long id;
-    @Column(name = "name")
-    private String username;
-    private Integer age;
-    @Enumerated(EnumType.STRING)
-    private RoleType roleType;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastModifiedDate;
-    @Lob
-    private String description;
+    @Column(name = "USERNAME")
+    private String name;
+    //@Column(name = "TEAM_ID")
+    //private Long teamId;
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
-    public Member(){
+    public Long getId() {
+        return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /*public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }*/
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
